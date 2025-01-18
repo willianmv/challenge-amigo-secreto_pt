3b2a1285ea1f09d21950147901539bc6f203cdb3
willianmv <willianvmarques300@gmail.com>
2025-01-18 15:31:58 -0300

    Adicionando lógica do desafio

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo(){
+    let input = document.getElementById("amigo").value;
+    
+    if(input==''){
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    listaAmigos.push(input);
+    atualizaLista();
+    limparInput();
+    console.log(listaAmigos);
+}
+
+function atualizaLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = '';
+
+    for(let i = 0; i < listaAmigos.length; i++){
+        let item = document.createElement("li");
+        item.textContent = listaAmigos[i];
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if(listaAmigos.length===0){
+        alert('Adicione amigos à lista para realizar o sorteio!');
+        return;
+    }
+
+    let numeroAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    console.log(numeroAleatorio);
+
+    let amigoSorteado = listaAmigos[numeroAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = amigoSorteado;
+
+    listaAmigos.splice(numeroAleatorio, 1);
+    atualizaLista();
+}
+
+function limparInput(){
+    let input = document.getElementById("amigo");
+    input.value = '';
+}
+
+
